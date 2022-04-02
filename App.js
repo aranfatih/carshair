8ca9df2bae9e3f3eb49a2e6be18cb2d23238fcb8
Aran Fatih <othman.aran@gmail.com>
2022-04-02 13:26:34 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
+import axios, * as others from 'axios';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './src/components/search';
+import './'
+import { colors } from './styles/colors';
+import Navbar from './src/components/layout/Navbar';
+import ImagePlaceholder from './src/components/layout/ImagePlaceholder';
+import Footer from './src/components/layout/Footer';
+import MorePlaceholder from './src/components/layout/MorePlaceholder';
+import Divider from './src/components/reusable/cards/Divider';
 
 export default function App() {
+  const [data, setData] = React.useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Navbar />
+      <Footer />
+      <ImagePlaceholder />
+      <MorePlaceholder />
+      <Search />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: '100%',
+    backgroundColor: colors.bg,
   },
 });
